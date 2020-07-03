27d8d20987bd7dad30da51a76ee5326b94040868
Joe Stringer <joe@wand.net.nz>
2020-07-03 15:26:47 -0700

    Add support for direct install / debug
    Usage:
    * Add the following to .env:
      MC_PATH='C:\Users\<USERNAME>\AppData\Roaming\.minecraft'
    * yarn build
    
    Yarn will install the scripts directly into your minecraft worldedit
    craftscripts directory.
    
    By default, this will be in tinyified form. For better debugging, define
    DEBUG=true in .env.
    
    Signed-off-by: Joe Stringer <joe@wand.net.nz>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv')
 const files = glob.sync('src/*.js')
 dotenv.config()
 
+const weditPath = (process.env.MC_PATH === undefined) ? __dirname : path.resolve(process.env.MC_PATH, './config/worldedit')
 for (let i = 0; i < files.length; i++) {
   task(path.basename(files[i], '.js'), () => {
     return bundle(files[i])
@@ -24,7 +25,7 @@ const transform = [
   }]
 ]
 
-const plugin = [
+const plugin = process.env.DEBUG ? [] : [
   ['tinyify', {
     env: { IGN_API_KEY: process.env.IGN_API_KEY || 'choisirgeoportail' }
   }]
@@ -33,17 +34,17 @@ const plugin = [
 function bundle (file) {
   return src(file)
     .pipe(bro({ transform, plugin }))
-    .pipe(dest('craftscripts/'))
+    .pipe(dest(path.resolve(weditPath, './craftscripts')))
 }
 
 task('copyData', (cb) => {
-  fs.mkdir(path.resolve(__dirname, './craftscripts'), (err) => {
+  fs.mkdir(path.resolve(weditPath, './craftscripts'), (err) => {
     if (err && err.code !== 'EEXIST') throw err
-    fs.mkdir(path.resolve(__dirname, './craftscripts/data'), (err) => {
+    fs.mkdir(path.resolve(weditPath, './craftscripts/data'), (err) => {
       if (err && err.code !== 'EEXIST') throw err
-      fs.copyFile(path.resolve(__dirname, './src/data/conformal.txt'), path.resolve(__dirname, './craftscripts/data/conformal.txt'), (err) => {
+      fs.copyFile(path.resolve(__dirname, './src/data/conformal.txt'), path.resolve(weditPath, './craftscripts/data/conformal.txt'), (err) => {
         if (err) throw err
-        fs.copyFile(path.resolve(__dirname, './src/config.json'), path.resolve(__dirname, './craftscripts/config.json'), (err) => {
+        fs.copyFile(path.resolve(__dirname, './src/config.json'), path.resolve(weditPath, './craftscripts/config.json'), (err) => {
           if (err) throw err
           cb()
         })
